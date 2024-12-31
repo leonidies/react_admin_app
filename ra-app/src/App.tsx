560b18b30b4656b92ecdb42570e679d6a8659925
leonidies <rohitcodes7888@gmail.com>
2024-12-31 11:52:15 +0530

    first commit

---
+++ ra-app/src/App.tsx
@@ -0,0 +1,30 @@
+import { Admin, Resource } from "react-admin";
+import jsonServerProvider from "ra-data-json-server";
+import { CategoryCreate} from "../src/components/CategoryCreate";
+import { CategoryEdit  } from "../src/components/CategoryEdit";
+import { CategoryList } from "../src/components/CategoryList";
+import { PosterList } from "../src/components/PosterList";
+import { PosterCreate } from "../src/components/PosterCreate";
+import { PosterEdit } from "../src/components/PosterEdit";
+
+const dataProvider = jsonServerProvider("http://localhost:3000");
+
+const App = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource
+      name="posters"
+      list={PosterList}
+      edit={PosterEdit}
+      create={PosterCreate}
+    />
+    <Resource
+      name="categories"
+      list={CategoryList}
+      edit={CategoryEdit}
+      create={CategoryCreate}
+      // recordRepresentation="name"
+    />
+  </Admin>
+);
+
+export default App;
